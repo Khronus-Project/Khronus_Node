9982c2990acbbc8cd30713ff5345c5ad84f12e5e
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-08-13 17:35:02 +0800

    able to listen to events and record events and alerts in database

--- khron_node/data/database.py
+++ khron_node/data/database.py
@@ -1,5 +1,14 @@
+from posixpath import dirname
 from sqlalchemy import create_engine
+from os import mkdir, path, environ
+from dotenv import load_dotenv
 
+load_dotenv()
+if not path.exists(environ['DATABASE_PATH']):
+    mkdir(environ['DATABASE_PATH'])
+    print(f"directory {environ['DATABASE_PATH']} created")
 
-engine = create_engine('sqlite:///khron_node/data/database.db', echo=True)
+db_path = f"sqlite:///{environ['DATABASE_PATH']}/database.db"
+
+engine = create_engine(db_path, echo=True)
 
